9479bafe8d9ba640ebdf2ba225e2f97b037472bc
ghdmsrkd <ghdmsrkd@naver.com>
2020-07-15 18:29:27 +0900

    Create 가운데글자가져오기.go

---
+++ argolism/가운데글자가져오기.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func solution(s string) string {
+	if len(s)%2 == 0 {
+		return s[len(s)/2-1 : len(s)/2+1]
+	} else {
+		return s[len(s)/2 : len(s)/2+1]
+	}
+}
+
+func solution1(s string) string {
+	return s[int(float64(len(s))/2-0.5):int(float64(len(s))/2+1)]
+}
+
+func main() {
+	fmt.Println(solution("abcd"))
+	fmt.Println(solution1("abcd"))
+}
