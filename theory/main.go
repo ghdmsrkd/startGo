5e8c87d42bfbc40c422b787879916107c0bc4e7f
victor <ghdmsrkd@naver.com>
2022-10-16 01:14:38 +0900

    variables and contants

--- theory/main.go
+++ theory/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!!")
+	a := 1
+	var b = 2
+	var c int = 3
+	fmt.Println(a, b, c)
+
+	aa := 1
+	const bb = 2
+	const cc = 3
+	fmt.Println(aa,bb)
 }
